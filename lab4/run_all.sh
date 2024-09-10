1ef230bd491b0a8767860471e8780ee4a2b5e68d
samuel.luna <samuel.luna@ccc.ufcg.edu.br>
2024-09-10 15:31:29 -0300

    adding run script for concurrent similarity

--- lab4/run_all.sh
+++ lab4/run_all.sh
@@ -5,4 +5,5 @@ args=`find dataset -type f | xargs`
 time bash go/serial/run.sh $args
 time bash go/concurrent-0/run.sh $args
 time bash go/serial-partial/run.sh $args
+time bash go/concurrent-partial/run.sh $args
 
